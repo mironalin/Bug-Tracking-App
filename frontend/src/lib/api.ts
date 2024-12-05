d15462fd0485a433d316412a62d1600be8aad1cb
mironalin <alinmiron3@gmail.com>
2024-12-04 19:53:36 -0800

    BACKEND: Prepare for server session management

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,17 @@
 import { hc } from "hono/client";
 import type { ApiRoutes } from "@server/app";
 
-const client = hc<ApiRoutes>("http://localhost:3000");
+const client = hc<ApiRoutes>("http://localhost:3000", {
+  fetch: (input: RequestInfo | URL, init?: RequestInit) => {
+    return fetch(input, {
+      ...init,
+      credentials: "include",
+      headers: {
+        ...init?.headers,
+        "Content-Type": "application/json",
+      },
+    });
+  },
+});
 
 export const api = client.api;
