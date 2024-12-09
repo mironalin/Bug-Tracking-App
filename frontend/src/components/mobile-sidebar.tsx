d3079ed53519602446d369beafda9d5936e1e67a
mironalin <alinmiron3@gmail.com>
2024-12-09 15:36:49 -0800

    FRONTEND: Add the Mobile Sidebar component

---
+++ frontend/src/components/mobile-sidebar.tsx
@@ -0,0 +1,26 @@
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sidebar } from "./sidebar";
+import { useEffect, useState } from "react";
+import { useLocation } from "@tanstack/react-router";
+
+export const MobileSidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = useLocation();
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+  return (
+    <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
+      <SheetTrigger asChild>
+        <Button size="icon" variant="secondary" className="lg:hidden">
+          <MenuIcon className="size-4 text-neutral-500" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
