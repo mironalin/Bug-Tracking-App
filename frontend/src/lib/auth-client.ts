815236f6fc815db120fcc18f4bf965a8dfbc4fca
mironalin <alinmiron3@gmail.com>
2024-12-09 15:37:16 -0800

    FRONTEND: AuthContext type is no longer needed

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -2,5 +2,3 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient();
 
 export const { signIn, signOut, signUp, useSession } = authClient;
-
-export type AuthContext = ReturnType<typeof useSession>;
