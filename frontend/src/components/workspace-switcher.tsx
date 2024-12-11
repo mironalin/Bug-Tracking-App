7538674353648dc41b44dc3bb7884768f19d3524
mironalin <alinmiron3@gmail.com>
2024-12-10 17:37:04 -0800

    FRONTEND: Add dynamic workspace routes

--- frontend/src/components/workspace-switcher.tsx
+++ frontend/src/components/workspace-switcher.tsx
@@ -4,9 +4,18 @@ import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { WorkspaceTypeInterface } from "@server/sharedTypes";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useNavigate } from "@tanstack/react-router";
+import { useParams } from "@tanstack/react-router";
 
 export const WorkspaceSwitcher = () => {
+  const { workspaceId } = useParams({ strict: false });
+
   const { data } = useGetWorkspaces();
+  const navigate = useNavigate();
+
+  const onSelect = (id: string) => {
+    navigate({ to: `/workspaces/${id}` });
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -14,13 +23,13 @@ export const WorkspaceSwitcher = () => {
         <p className="text-xs uppercase text-neutral-500">Workspaces</p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
           <SelectValue placeholder="No workspace selected"></SelectValue>
         </SelectTrigger>
         <SelectContent>
           {data?.workspaces.map((workspace: WorkspaceTypeInterface) => (
-            <SelectItem key={workspace.id} value={String(workspace.id)}>
+            <SelectItem key={workspace.id} value={String(workspace.slug)}>
               <div className="flex justify-start items-center gap-3 font-medium">
                 <WorkspaceAvatar name={workspace.name} image={workspace.imageUrl} />
                 <span className="truncate">{workspace.name}</span>
---
+++ frontend/src/routes/_authenticated/workspaces/$workspaceId.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_authenticated/workspaces/$workspaceId")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { workspaceId } = Route.useParams();
+  return <div>{`Workspace id: ${workspaceId}`}</div>;
+}
