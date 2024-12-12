a3803276da2262f97db99218fd07b52c10edcb39
mironalin <alinmiron3@gmail.com>
2024-12-11 17:21:04 -0800

    FRONTEND: Fix issues regarding accessibility

--- frontend/src/components/mobile-sidebar.tsx
+++ frontend/src/components/mobile-sidebar.tsx
@@ -1,9 +1,10 @@
 import { MenuIcon } from "lucide-react";
 import { Button } from "./ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { Sidebar } from "./sidebar";
 import { useEffect, useState } from "react";
 import { useLocation } from "@tanstack/react-router";
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 
 export const MobileSidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +19,10 @@ export const MobileSidebar = () => {
           <MenuIcon className="size-4 text-neutral-500" />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="p-0">
+      <SheetContent aria-describedby={undefined} side="left" className="p-0">
+        <VisuallyHidden.Root>
+          <SheetTitle>Drawer</SheetTitle>
+        </VisuallyHidden.Root>
         <Sidebar />
       </SheetContent>
     </Sheet>
--- frontend/src/components/responsive-modal.tsx
+++ frontend/src/components/responsive-modal.tsx
@@ -1,8 +1,10 @@
 import { useMedia } from "react-use";
 
-import { Dialog, DialogContent } from "./ui/dialog";
+import { Dialog, DialogContent, DialogTitle } from "./ui/dialog";
 
-import { Drawer, DrawerContent } from "./ui/drawer";
+import { Drawer, DrawerContent, DrawerTitle } from "./ui/drawer";
+
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 
 interface ResponsiveModalProps {
   children: React.ReactNode;
@@ -11,12 +13,18 @@ interface ResponsiveModalProps {
 }
 
 export const ResponsiveModal = ({ children, open, onOpenChange }: ResponsiveModalProps) => {
-  const isDesktop = useMedia("(min-width: 1024px)", true);
+  const isDesktop = useMedia("(min-width: 1024px)");
 
   if (isDesktop) {
     return (
       <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent className="w-full sm:max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh]">
+        <DialogContent
+          aria-describedby={undefined}
+          className="w-full sm:max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh]"
+        >
+          <VisuallyHidden.Root>
+            <DialogTitle>Dialog</DialogTitle>
+          </VisuallyHidden.Root>
           {children}
         </DialogContent>
       </Dialog>
@@ -25,7 +33,10 @@ export const ResponsiveModal = ({ children, open, onOpenChange }: ResponsiveModa
 
   return (
     <Drawer open={open} onOpenChange={onOpenChange}>
-      <DrawerContent>
+      <DrawerContent aria-describedby={undefined}>
+        <VisuallyHidden.Root>
+          <DrawerTitle>Drawer</DrawerTitle>
+        </VisuallyHidden.Root>
         <div className="overflow-y-auto hide-scrollbar max-h-[85vh]">{children}</div>
       </DrawerContent>
     </Drawer>
