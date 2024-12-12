4dad1eb07b0c3bd60e0ebb1049393e8f549143f4
mironalin <alinmiron3@gmail.com>
2024-12-12 05:14:05 -0800

    BACKEND: Remove unnecessary import

--- backend/src/db/schema/workspaces-schema.ts
+++ backend/src/db/schema/workspaces-schema.ts
@@ -1,7 +1,6 @@
 // import * as _1 from "../../../node_modules/drizzle-zod/schema.types.internal.mjs";
 
 import { pgTable, text, integer, timestamp, index, varchar } from "drizzle-orm/pg-core";
-import { sql } from "drizzle-orm";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { z } from "zod";
 export const workspaces = pgTable(
