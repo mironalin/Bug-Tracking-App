a5114db86a4d71d23542a4954978bda2987cffb7
mironalin <alinmiron3@gmail.com>
2024-12-24 07:36:26 -0800

    BACKEND: Update Projects Schema

--- backend/src/db/schema/projects-schema.ts
+++ backend/src/db/schema/projects-schema.ts
@@ -1,9 +1,12 @@
 import { pgTable, text, integer, timestamp, index, varchar } from "drizzle-orm/pg-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const projects = pgTable("projects", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  slug: varchar().$default(() => generateUniqueString(16)),
+  id: integer().primaryKey().generatedAlwaysAsIdentity().notNull(),
+  slug: varchar()
+    .$default(() => generateUniqueString(16))
+    .notNull(),
   name: text("name").notNull(),
   workspaceId: varchar().notNull(),
   imageUrl: text("imageUrl"),
@@ -13,9 +16,21 @@ export const projects = pgTable("projects", {
     .$onUpdate(() => new Date()),
 });
 
-export const insertProjectsSchema = createInsertSchema(projects);
+export const insertProjectsSchema = createInsertSchema(projects, {
+  slug: z.string().optional(),
+  name: z.string().trim().min(1, "Required"),
+  workspaceId: z.string().trim().min(1, "Required"),
+  imageUrl: z
+    .string()
+    .transform((value) => (value === "" ? undefined : value))
+    .optional(),
+  createdAt: z.date().default(() => new Date()),
+  updatedAt: z.date().default(() => new Date()),
+});
 
 export const selectProjectsSchema = createSelectSchema(projects);
+
+export const updateProjectsSchema = createInsertSchema(projects).pick({ name: true, imageUrl: true }).partial();
 
 export function generateUniqueString(length: number = 12): string {
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
