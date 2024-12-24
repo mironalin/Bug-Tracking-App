f6bffc18506703343fa334bed4668b2ce9407543
mironalin <alinmiron3@gmail.com>
2024-12-24 08:48:18 -0800

    BACKEND: Add Patch and Delete api endpoints for Projects

--- backend/src/routes/projects.ts
+++ backend/src/routes/projects.ts
@@ -6,7 +6,7 @@ import { zValidator } from "@hono/zod-validator";
 
 import { getSessionAndUser } from "../middleware/get-session-and-user.js";
 import { db } from "../db/index.js";
-import { insertProjectsSchema, projects as projectsTable } from "../db/schema/projects-schema.js";
+import { insertProjectsSchema, projects as projectsTable, updateProjectsSchema } from "../db/schema/projects-schema.js";
 import { getMember } from "../lib/utils.js";
 
 export const projectsRoute = new Hono()
@@ -73,4 +73,52 @@ export const projectsRoute = new Hono()
     }
 
     return c.json({ project });
+  })
+  .patch("/:projectId", getSessionAndUser, zValidator("json", updateProjectsSchema), async (c) => {
+    const user = c.var.user;
+    if (!user) return c.body(null, 401);
+
+    const { name, imageUrl } = c.req.valid("json");
+
+    const { projectId } = c.req.param();
+
+    const existingProject = (await db.select().from(projectsTable).where(eq(projectsTable.slug, projectId)))[0];
+
+    const member = await getMember({ db, workspaceId: existingProject.workspaceId, userId: user.id });
+
+    if (!member) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    const validatedProjectUpdate = updateProjectsSchema.parse({
+      name: name,
+      imageUrl: imageUrl ?? null,
+    });
+
+    console.log("Validated Update Object:", validatedProjectUpdate);
+
+    const updatedProject = await db
+      .update(projectsTable)
+      .set(validatedProjectUpdate)
+      .where(eq(projectsTable.slug, projectId));
+
+    return c.json({ updatedProject });
+  })
+  .delete("/:projectId", getSessionAndUser, async (c) => {
+    const user = c.var.user;
+    if (!user) return c.body(null, 401);
+
+    const { projectId } = c.req.param();
+
+    const existingProject = (await db.select().from(projectsTable).where(eq(projectsTable.slug, projectId)))[0];
+
+    const member = await getMember({ db, workspaceId: existingProject.workspaceId, userId: user.id });
+
+    if (!member) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    await db.delete(projectsTable).where(eq(projectsTable.slug, projectId));
+
+    return c.json({ project: { slug: projectId } });
   });
--- backend/src/sharedTypes.ts
+++ backend/src/sharedTypes.ts
@@ -4,7 +4,7 @@ import {
   updateWorkspacesSchema,
 } from "./db/schema/workspaces-schema.js";
 import { selectMembersSchema } from "./db/schema/members-schema.js";
-import { insertProjectsSchema, selectProjectsSchema } from "./db/schema/projects-schema.js";
+import { insertProjectsSchema, selectProjectsSchema, updateProjectsSchema } from "./db/schema/projects-schema.js";
 import { z } from "zod";
 
 export const createWorkspaceSchema = insertWorkspacesSchema.omit({
@@ -16,9 +16,17 @@ export const createWorkspaceSchema = insertWorkspacesSchema.omit({
   inviteCode: true,
 });
 
+export const createProjectSchema = insertProjectsSchema.omit({
+  createdAt: true,
+  updatedAt: true,
+  id: true,
+  slug: true,
+});
+
 export type CreateWorkspace = z.infer<typeof createWorkspaceSchema>;
 export type UpdateWorkspace = z.infer<typeof updateWorkspacesSchema>;
-export type CreateProject = z.infer<typeof insertProjectsSchema>;
+export type CreateProject = z.infer<typeof createProjectSchema>;
+export type UpdateProject = z.infer<typeof updateProjectsSchema>;
 export type WorkspaceTypeInterface = z.infer<typeof selectWorkspacesSchema>;
 export type MemberTypeInterface = z.infer<typeof selectMembersSchema>;
 export type ProjectTypeInterface = z.infer<typeof selectProjectsSchema>;
