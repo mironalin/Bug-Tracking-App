b0138fc57d63afc3574c091f194cea4536efc518
mironalin <alinmiron3@gmail.com>
2024-12-24 05:42:00 -0800

    BACKEND: Remove auth CORS for now

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,19 +11,19 @@ import { projectsRoute } from "./routes/projects.js";
 
 const app = new Hono();
 
-app.use(
-  "/api/auth/**", // or replace with "*" to enable cors for all routes
-  cors({
-    origin: ["http://localhost:3000"],
-    allowHeaders: ["Content-Type", "Authorization"],
-    allowMethods: ["POST", "GET", "OPTIONS"],
-    exposeHeaders: ["Content-Length"],
-    maxAge: 600,
-    credentials: true,
-  })
-);
+// app.use(
+//   "/api/auth/**", // or replace with "*" to enable cors for all routes
+//   cors({
+//     origin: ["http://localhost:3000"],
+//     allowHeaders: ["Content-Type", "Authorization"],
+//     allowMethods: ["POST", "GET", "OPTIONS"],
+//     exposeHeaders: ["Content-Length"],
+//     maxAge: 600,
+//     credentials: true,
+//   })
+// );
 
-// app.use("*", logger());
+app.use("*", logger());
 
 const apiRoutes = app
   .basePath("/api")
