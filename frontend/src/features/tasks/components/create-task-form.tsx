4ba05d198e8b5daea8647b31f99f17825114d8a3
mironalin <alinmiron3@gmail.com>
2025-01-15 18:03:05 -0800

    FRONTEND: Remove react-hook-form DevTools from the tasks form

--- frontend/src/features/tasks/components/create-task-form.tsx
+++ frontend/src/features/tasks/components/create-task-form.tsx
@@ -1,7 +1,6 @@
 import { useForm } from "react-hook-form";
 import { cn } from "@/lib/utils";
 import { z } from "zod";
-import { DevTool } from "@hookform/devtools";
 
 import { useCreateTask } from "../api/use-create-task";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -54,144 +53,141 @@ export const CreateTaskForm = ({ onCancel, projectOptions, memberOptions }: Crea
   // const onInvalid = (errors) => console.error(errors);
 
   return (
-    <>
-      <Card className="w-full h-full border-none shadow-none">
-        <CardHeader className="flex p-7">
-          <CardTitle className="text-xl font-bold">Create a new task</CardTitle>
-        </CardHeader>
-        <div className="px-7">
-          <DottedSeparator />
-        </div>
-        <CardContent className="p-7">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
-              <div className="flex flex-col gap-y-4">
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Task name</FormLabel>
+    <Card className="w-full h-full border-none shadow-none">
+      <CardHeader className="flex p-7">
+        <CardTitle className="text-xl font-bold">Create a new task</CardTitle>
+      </CardHeader>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Task name</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Enter task name" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="dueDate"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Due Date</FormLabel>
+                    <FormControl>
+                      <DatePicker {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="assigneeId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Assignee</FormLabel>
+                    <Select defaultValue={field.value} onValueChange={field.onChange}>
                       <FormControl>
-                        <Input {...field} placeholder="Enter task name" />
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select assignee" />
+                        </SelectTrigger>
                       </FormControl>
                       <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="dueDate"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Due Date</FormLabel>
+                      <SelectContent>
+                        {memberOptions.map((member) => (
+                          <SelectItem key={member.slug} value={member.slug}>
+                            <div className="flex items-center gap-x-2">
+                              <MemberAvatar className="size-6" name={member.name} />
+                              {member.name}
+                            </div>
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="status"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Status</FormLabel>
+                    <Select defaultValue={field.value} onValueChange={field.onChange}>
                       <FormControl>
-                        <DatePicker {...field} />
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select status" />
+                        </SelectTrigger>
                       </FormControl>
                       <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="assigneeId"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Assignee</FormLabel>
-                      <Select defaultValue={field.value} onValueChange={field.onChange}>
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select assignee" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <FormMessage />
-                        <SelectContent>
-                          {memberOptions.map((member) => (
-                            <SelectItem key={member.slug} value={member.slug}>
-                              <div className="flex items-center gap-x-2">
-                                <MemberAvatar className="size-6" name={member.name} />
-                                {member.name}
-                              </div>
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="status"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Status</FormLabel>
-                      <Select defaultValue={field.value} onValueChange={field.onChange}>
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select status" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <FormMessage />
-                        <SelectContent>
-                          <SelectItem value={TaskStatus.BACKLOG}>Backlog</SelectItem>
-                          <SelectItem value={TaskStatus.IN_PROGRESS}>In Progress</SelectItem>
-                          <SelectItem value={TaskStatus.IN_REVIEW}>In Review</SelectItem>
-                          <SelectItem value={TaskStatus.TODO}>Todo</SelectItem>
-                          <SelectItem value={TaskStatus.DONE}>Done</SelectItem>
-                        </SelectContent>
-                      </Select>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="projectId"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Project</FormLabel>
-                      <Select defaultValue={field.value} onValueChange={field.onChange}>
-                        <FormControl>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Select project" />
-                          </SelectTrigger>
-                        </FormControl>
-                        <FormMessage />
-                        <SelectContent>
-                          {projectOptions.map((project) => (
-                            <SelectItem key={project.slug} value={project.slug}>
-                              <div className="flex items-center gap-x-2">
-                                <ProjectAvatar className="size-6" name={project.name} image={project.imageUrl} />
-                                {project.name}
-                              </div>
-                            </SelectItem>
-                          ))}
-                        </SelectContent>
-                      </Select>
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <DottedSeparator className="py-7" />
-              <div className="flex items-center justify-between">
-                <Button
-                  type="button"
-                  size="lg"
-                  variant="secondary"
-                  onClick={onCancel}
-                  disabled={isPending}
-                  className={cn(onCancel ? "block" : "invisible")}
-                >
-                  Cancel
-                </Button>
-                <Button type="submit" size="lg" variant="primary" disabled={isPending}>
-                  {isPending ? "Creating Task..." : "Create Task"}
-                </Button>
-              </div>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-      <DevTool control={form.control} />
-    </>
+                      <SelectContent>
+                        <SelectItem value={TaskStatus.BACKLOG}>Backlog</SelectItem>
+                        <SelectItem value={TaskStatus.IN_PROGRESS}>In Progress</SelectItem>
+                        <SelectItem value={TaskStatus.IN_REVIEW}>In Review</SelectItem>
+                        <SelectItem value={TaskStatus.TODO}>Todo</SelectItem>
+                        <SelectItem value={TaskStatus.DONE}>Done</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="projectId"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Project</FormLabel>
+                    <Select defaultValue={field.value} onValueChange={field.onChange}>
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select project" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <FormMessage />
+                      <SelectContent>
+                        {projectOptions.map((project) => (
+                          <SelectItem key={project.slug} value={project.slug}>
+                            <div className="flex items-center gap-x-2">
+                              <ProjectAvatar className="size-6" name={project.name} image={project.imageUrl} />
+                              {project.name}
+                            </div>
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DottedSeparator className="py-7" />
+            <div className="flex items-center justify-between">
+              <Button
+                type="button"
+                size="lg"
+                variant="secondary"
+                onClick={onCancel}
+                disabled={isPending}
+                className={cn(onCancel ? "block" : "invisible")}
+              >
+                Cancel
+              </Button>
+              <Button type="submit" size="lg" variant="primary" disabled={isPending}>
+                {isPending ? "Creating Task..." : "Create Task"}
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
