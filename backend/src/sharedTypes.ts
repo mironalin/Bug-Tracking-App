8d2ac08a4b96ea5ae771d5b13b7f7099c2dccac4
mironalin <alinmiron3@gmail.com>
2025-01-15 17:47:28 -0800

    BACKEND: Update the sharedTypes for Tasks

--- backend/src/sharedTypes.ts
+++ backend/src/sharedTypes.ts
@@ -5,6 +5,7 @@ import {
 } from "./db/schema/workspaces-schema.js";
 import { selectMembersSchema } from "./db/schema/members-schema.js";
 import { insertProjectsSchema, selectProjectsSchema, updateProjectsSchema } from "./db/schema/projects-schema.js";
+import { insertTasksSchema } from "./db/schema/tasks-schema.js";
 import { z } from "zod";
 
 export const createWorkspaceSchema = insertWorkspacesSchema.omit({
@@ -23,10 +24,18 @@ export const createProjectSchema = insertProjectsSchema.omit({
   slug: true,
 });
 
+export const createTasksSchema = insertTasksSchema.omit({
+  createdAt: true,
+  updatedAt: true,
+  id: true,
+  slug: true,
+});
+
 export type CreateWorkspace = z.infer<typeof createWorkspaceSchema>;
 export type UpdateWorkspace = z.infer<typeof updateWorkspacesSchema>;
 export type CreateProject = z.infer<typeof createProjectSchema>;
 export type UpdateProject = z.infer<typeof updateProjectsSchema>;
+export type CreateTask = z.infer<typeof createTasksSchema>;
 export type WorkspaceTypeInterface = z.infer<typeof selectWorkspacesSchema>;
 export type MemberTypeInterface = z.infer<typeof selectMembersSchema>;
 export type ProjectTypeInterface = z.infer<typeof selectProjectsSchema>;
@@ -34,4 +43,12 @@ export type ProjectTypeInterface = z.infer<typeof selectProjectsSchema>;
 export enum MemberRole {
   ADMIN = "admin",
   MEMBER = "member",
+}
+
+export enum TaskStatus {
+  BACKLOG = "BACKLOG",
+  TODO = "TODO",
+  IN_PROGRESS = "IN_PROGRESS",
+  IN_REVIEW = "IN_REVIEW",
+  DONE = "DONE",
 }
