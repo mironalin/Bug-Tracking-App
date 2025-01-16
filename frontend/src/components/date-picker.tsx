4ed576419ef7d86dc4b53b2004b8671a24b09571
mironalin <alinmiron3@gmail.com>
2025-01-16 15:55:34 -0800

    FRONTEND: Allow date-picker customization

--- frontend/src/components/date-picker.tsx
+++ frontend/src/components/date-picker.tsx
@@ -13,16 +13,21 @@ interface DatePickerProps {
   value: Date | undefined;
   onChange: (date: Date) => void;
   placeholder?: string;
+  className?: string;
 }
 
-export const DatePicker = ({ value, onChange, placeholder = "Pick a date" }: DatePickerProps) => {
+export const DatePicker = ({ value, onChange, placeholder = "Pick a date", className }: DatePickerProps) => {
   return (
     <Popover>
       <PopoverTrigger asChild>
         <Button
           variant={"outline"}
           size={"lg"}
-          className={cn("w-full justify-start text-left font-normal px-3", !value && "text-muted-foreground")}
+          className={cn(
+            "w-full justify-start text-left font-normal px-3",
+            !value && "text-muted-foreground",
+            className
+          )}
         >
           <CalendarIcon />
           {value ? format(value, "PPP") : <span>{placeholder}</span>}
