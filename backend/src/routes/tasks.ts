b8388150f67c91b23eb9055945a9c68bd03e1141
mironalin <alinmiron3@gmail.com>
2025-01-18 04:09:18 -0800

    BACKEND: Add param GET method and a PATCH method for tasks

--- backend/src/routes/tasks.ts
+++ backend/src/routes/tasks.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { getSessionAndUser } from "../middleware/get-session-and-user.js";
 import { zValidator } from "@hono/zod-validator";
-import { insertTasksSchema, tasks as tasksTable } from "../db/schema/tasks-schema.js";
+import { insertTasksSchema, tasks as tasksTable, updateTasksSchema } from "../db/schema/tasks-schema.js";
 import { projects as projectsTable } from "../db/schema/projects-schema.js";
 import { members as membersTable } from "../db/schema/members-schema.js";
 import { user as userTable } from "../db/schema/auth-schema.js";
@@ -106,6 +106,52 @@ export const tasksRoute = new Hono()
       });
     }
   )
+  .get("/:taskSlug", getSessionAndUser, async (c) => {
+    const currentUser = c.var.user;
+    if (!currentUser) return c.body(null, 401);
+
+    const users = await db.select().from(userTable);
+
+    const { taskSlug } = c.req.param();
+
+    const task = await db
+      .select()
+      .from(tasksTable)
+      .where(eq(tasksTable.slug, taskSlug))
+      .then((res) => res[0]);
+
+    const currentMember = await getMember({ db, workspaceId: task.workspaceId, userId: currentUser.id });
+
+    if (!currentMember) return c.json({ error: "Unauthorized" }, 401);
+
+    const project = await db
+      .select()
+      .from(projectsTable)
+      .where(eq(projectsTable.slug, task.projectId))
+      .then((res) => res[0]);
+
+    const member = await db
+      .select()
+      .from(membersTable)
+      .where(eq(membersTable.slug, task.assigneeId))
+      .then((res) => res[0]);
+
+    const user = await db
+      .select()
+      .from(userTable)
+      .where(eq(userTable.id, member.userId))
+      .then((res) => res[0]);
+
+    const assignee = { ...member, name: user.name, email: user.email };
+
+    return c.json({
+      data: {
+        ...task,
+        project,
+        assignee,
+      },
+    });
+  })
   .post("/", getSessionAndUser, zValidator("json", insertTasksSchema), async (c) => {
     const user = c.var.user;
     if (!user) return c.body(null, 401);
@@ -146,4 +192,69 @@ export const tasksRoute = new Hono()
       .then((res) => res[0]);
 
     return c.json(result);
+  })
+  .patch("/:taskId", getSessionAndUser, zValidator("json", insertTasksSchema.partial()), async (c) => {
+    const user = c.var.user;
+    if (!user) return c.body(null, 401);
+
+    const { name, status, projectId, dueDate, assigneeId, description } = c.req.valid("json");
+    const { taskId } = c.req.param();
+
+    const existingTask = await db
+      .select()
+      .from(tasksTable)
+      .where(eq(tasksTable.slug, taskId))
+      .then((res) => res[0]);
+
+    const member = await getMember({
+      db,
+      userId: user.id,
+      workspaceId: existingTask.workspaceId,
+    });
+
+    if (!member) return c.json({ error: "Unauthorized" }, 401);
+
+    const validatedTaskUpdate = updateTasksSchema.parse({
+      name: name,
+      status: status,
+      projectId: projectId,
+      dueDate: dueDate,
+      assigneeId: assigneeId,
+      description: description,
+    });
+
+    console.log("dueDate: ", dueDate);
+
+    const task = await db
+      .update(tasksTable)
+      .set(validatedTaskUpdate)
+      .where(eq(tasksTable.slug, taskId))
+      .returning()
+      .then((res) => res[0]);
+
+    return c.json({ data: task });
+  })
+  .delete("/:taskId", getSessionAndUser, async (c) => {
+    const user = c.var.user;
+    if (!user) return c.body(null, 401);
+
+    const { taskId } = c.req.param();
+
+    const task = await db
+      .select()
+      .from(tasksTable)
+      .where(eq(tasksTable.slug, taskId))
+      .then((res) => res[0]);
+
+    const member = await getMember({ db, workspaceId: task.workspaceId, userId: user.id });
+
+    if (!member) return c.json({ error: "Unauthorized" }, 401);
+
+    await db.delete(tasksTable).where(eq(tasksTable.slug, taskId));
+
+    return c.json({
+      data: {
+        slug: taskId,
+      },
+    });
   });
