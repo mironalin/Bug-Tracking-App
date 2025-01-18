461080c9014985c93663db345ae6e15551569a51
mironalin <alinmiron3@gmail.com>
2025-01-18 04:09:48 -0800

    FRONTEND: useGetTask hook

--- frontend/src/features/tasks/api/use-get-task.ts
+++ frontend/src/features/tasks/api/use-get-task.ts
@@ -2,40 +2,30 @@ import { useQuery } from "@tanstack/react-query";
 
 import { api } from "@/lib/rpc";
 
-interface GetTasksQuery {
-  workspaceId: string;
-  projectId?: string | null;
-  status?: string | null;
-  search?: string | null;
-  assigneeId?: string | null;
-  dueDate?: string | null;
+interface GetTaskQuery {
+  taskSlug: string;
 }
 
-export const getTasksQuery = async ({ workspaceId, projectId, status, search, assigneeId, dueDate }: GetTasksQuery) => {
-  const response = await api.tasks.$get({
-    query: {
-      workspaceId,
-      projectId: projectId ?? undefined,
-      status: status ?? undefined,
-      search: search ?? undefined,
-      assigneeId: assigneeId ?? undefined,
-      dueDate: dueDate ?? undefined,
+export const getTaskQuery = async ({ taskSlug }: GetTaskQuery) => {
+  const response = await api.tasks[":taskSlug"].$get({
+    param: {
+      taskSlug,
     },
   });
 
   if (!response.ok) {
-    throw Error("Failed to fetch tasks: " + response.statusText);
+    throw Error("Failed to individual task: " + response.statusText);
   }
 
-  const data = await response.json();
+  const { data } = await response.json();
 
   return data;
 };
 
-export const useGetTasks = ({ workspaceId, projectId, status, search, assigneeId, dueDate }: GetTasksQuery) => {
+export const useGetTask = ({ taskSlug }: GetTaskQuery) => {
   const query = useQuery({
-    queryKey: ["tasks", workspaceId, projectId, status, search, assigneeId, dueDate],
-    queryFn: () => getTasksQuery({ workspaceId, projectId, status, search, assigneeId, dueDate }),
+    queryKey: ["task", taskSlug],
+    queryFn: () => getTaskQuery({ taskSlug }),
   });
   return query;
 };
