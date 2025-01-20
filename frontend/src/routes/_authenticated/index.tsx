112b6d76832264e2dd9e78e1884946ffe5939796
mironalin <alinmiron3@gmail.com>
2025-01-19 19:29:51 -0800

    FRONTEND: Simplify workspace redirection logic by updating data structure handling

--- frontend/src/routes/_authenticated/index.tsx
+++ frontend/src/routes/_authenticated/index.tsx
@@ -5,10 +5,10 @@ export const Route = createFileRoute("/_authenticated/")({
   beforeLoad: async ({ context }) => {
     const queryClient = context.queryClient;
     const data = await queryClient.fetchQuery(getWorkspacesQueryOptions);
-    if (data.workspaces.length === 0) {
+    if (data.length === 0) {
       throw redirect({ to: "/workspaces/create" });
     } else {
-      throw redirect({ to: `/workspaces/${data.workspaces[data.workspaces.length - 1].slug}` });
+      throw redirect({ to: `/workspaces/${data[data.length - 1].slug}` });
     }
   },
   component: Index,
